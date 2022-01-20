11dd1601cf63c62707d6ad7d5709ddcb62b7826f
jason <x@x.com>
2022-01-19 23:09:19 -0500

    modify pragma

--- faucet.sol
+++ faucet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC-BY-SA-4.0
 
 // Version of Solidity compiler this program was written for
-pragma solidity 0.8.11+commit.d7f03943.Emscripten.clang;
+pragma solidity 0.8.11;
 
 // Our first contract is a faucet!
 contract Faucet {
